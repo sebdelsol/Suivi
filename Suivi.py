f1530bd6a4813f5df1c0cf0d1997548cabb5613a
sebdelsol <seb.morin@gmail.com>
2022-01-28 01:43:23 +0100

    clean

--- Suivi.py
+++ Suivi.py
@@ -27,11 +27,11 @@ class Splash:
         self.window.close()
 
 
-if __name__ == "__main__":
-    needed_version = SpecifierSet(Python_version)
+if __name__ == "__main__":   
     version = ".".join(str(v) for v in sys.version_info[:3])
     print(f"Python {version} running")
-
+    
+    needed_version = SpecifierSet(Python_version)
     if version not in needed_version:
         needs = " and ".join(need for need in str(needed_version).split(","))
         print(f"Unfortunatly this app needs Python {needs}")
