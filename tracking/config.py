3749bc91fcb2aecc9339466a04211bae6e9a7509
sebdelsol <seb.morin@gmail.com>
2022-02-06 16:20:26 +0100

    patch chromedriver @ beginning

--- tracking/config.py
+++
@@ -1,2 +0,0 @@
-# add your actuel chrome path
-CHROME_EXE_PATH = r"C:/Program Files/Google/Chrome/Application/chrome.exe"
--- tracking/drivers.py
+++ tracking/drivers.py
@@ -1,7 +1,8 @@
 import atexit
+import os
 import queue
+import stat
 import threading
-import time
 import traceback
 from socket import error as SocketError
 
@@ -11,45 +12,97 @@ from selenium.common.exceptions import (
     SessionNotCreatedException,
     WebDriverException,
 )
+
+import undetected_chromedriver as webdriver
 from urllib3.exceptions import ProtocolError
+from win32com.client import Dispatch
+
 from windows.localization import TXT
 from windows.log import log
 
-from tracking.config import CHROME_EXE_PATH
-
-USE_UC_V2 = True
 CREATE_DRIVER_AT_INIT = False
 
-if USE_UC_V2:
-    import undetected_chromedriver as webdriver
 
-else:
-    import undetected_chromedriver._compat as webdriver
+def find_chrome_executable():
+    """fix find_chrome_executable for x86 Windows"""
+
+    candidates = set()
+    for item in map(
+        os.environ.get, ("PROGRAMFILES", "PROGRAMFILES(X86)", "LOCALAPPDATA")
+    ):
+        if item:
+            for subitem in (
+                "Google/Chrome/Application",
+                "Google/Chrome Beta/Application",
+                "Google/Chrome Canary/Application",
+            ):
+                candidates.add(os.sep.join((item, subitem, "chrome.exe")))
+    for candidate in candidates:
+        if os.path.exists(candidate) and os.access(candidate, os.X_OK):
+            return os.path.normpath(candidate)
+
+
+# monkey patch it
+webdriver.find_chrome_executable = find_chrome_executable
+
+
+def get_chrome_main_version():
+    """get installed Chrome main version number"""
+
+    filename = find_chrome_executable()
+    parser = Dispatch("Scripting.FileSystemObject")
+    version = parser.GetFileVersion(filename)
+    return version.split(".")[0]
+
+
+def patch_driver(version):
+    """patch chromedriver then lock it to avoid further patching and permission errors"""
+
+    log(f"PATCH chromedriver {version=}")
+    patcher = webdriver.Patcher(version_main=version)
+    # unlock chromdriver.exe and patch it
+    os.chmod(patcher.executable_path, stat.S_IREAD + stat.S_IWRITE)
+    patcher.auto()
+    # lock chromedriver.exe to prevent any further patch
+    os.chmod(patcher.executable_path, stat.S_IREAD)
 
 
 class _DriversHandler:
     name = None
+    _patching_lock = threading.Lock()
+    _patching_done = False
+    _chrome_main_version = get_chrome_main_version()
 
     def __init__(self):
+        self.max_drivers = float("inf")
+        self._n_in_creation = 0
         self._drivers = []
-        self.max_drivers = None
-        self._in_creation = 0
+        self._drivers_available = queue.Queue()
         self._creation_threads = []
         self._driver_count_ops = threading.Lock()
         atexit.register(self._close)
 
     def _log_creation(self, txt, n_drivers, error=False):
         msg = f"{self.name} {txt}"
-        if self.max_drivers:
-            msg += f" ({n_drivers + 1}/{self.max_drivers})"
+        msg += f" ({n_drivers + 1}/{self.max_drivers})"
         log(msg, error=error)
 
-    def create(self, create_driver_func):
+    @classmethod
+    def _patch_driver(cls):
+        """patch chromedriver only once, thread safe"""
+        with cls._patching_lock:
+            if not cls._patching_done:
+                patch_driver(cls._chrome_main_version)
+                cls._patching_done = True
+
+    def get_driver_options(self):
+        raise NotImplementedError("a driver needs options")
+
+    def create_driver(self):
         with self._driver_count_ops:
-            n_drivers = len(self._drivers) + self._in_creation
-            can_create = not self.max_drivers or n_drivers < self.max_drivers
-            if can_create:
-                self._in_creation += 1
+            n_drivers = len(self._drivers) + self._n_in_creation
+            if can_create := n_drivers < self.max_drivers:
+                self._n_in_creation += 1
                 thread = threading.current_thread()
                 self._creation_threads.append(thread)
 
@@ -57,7 +110,9 @@ class _DriversHandler:
             driver = None
             self._log_creation("start CREATION", n_drivers)
             try:
-                driver = create_driver_func(n_drivers)
+                _DriversHandler._patch_driver()
+                options = self.get_driver_options()
+                driver = webdriver.Chrome(options=options)
                 self._log_creation("has been CREATED", n_drivers)
 
             except (SessionNotCreatedException, ProtocolError, SocketError) as e:
@@ -66,20 +121,40 @@ class _DriversHandler:
             finally:
                 with self._driver_count_ops:
                     self._creation_threads.remove(thread)
-                    self._in_creation -= 1
+                    self._n_in_creation -= 1
                     if driver:
                         self._drivers.append(driver)
+                        self._drivers_available.put(driver)
 
             return driver
         return None
 
+    def get(self):
+        """try to get an available driver. if not try to create a driver then wait for an available one"""
+        try:
+            if driver := self._drivers_available.get(block=False):
+                return driver
+        except queue.Empty:
+            pass
+
+        # create a driver till max_drivers is reached
+        self.create_driver()
+        # wait for an available driver
+        # since the creation might fail or the driver could be stolen by another thread
+        return self._drivers_available.get()
+
+    def dispose(self, driver):
+        self._drivers_available.put(driver)
+
     def destroy(self, driver):
         with self._driver_count_ops:
             driver.quit()
+            log(f"QUIT {self.name}")
             self._drivers.remove(driver)
 
     def _close(self):
         log(f"CLOSING {self.name}")
+
         with self._driver_count_ops:
             n_drivers = len(self._drivers)
             for i, driver in enumerate(self._drivers):
@@ -97,104 +172,53 @@ class _DriversHandler:
 
 class DriversToScrape(_DriversHandler):
     max_drivers = 2
-    name = "chromedriver to scrape"
-
-    experimental_options = dict(
-        prefs={
-            "translate_whitelists": {
-                "de": "fr",
-                "es": "fr",
-                "en": "fr",
-                "und": "fr",
-                "zh-CN": "fr",
-                "zh-TW": "fr",
-            },
-            "translate": {"enabled": "true"},
-            "profile.managed_default_content_settings.images": 2,  # remove image
-            "profile.managed_default_content_settings.cookies": 2,  # remove cookies
-        },
-        excludeSwitches=["enable-logging"],
-    )
+    name = "Chromedriver (scrapper)"
 
     options = (
         "--no-first-run",
         "--no-service-autorun",
         "--password-store=basic",
         "--lang=fr",
-    )
-
-    options_V1 = ("--window-size=1024,768",)  # reach sliders
-    options_V2 = (
         "--excludeSwitches --enable-logging",
         "--blink-settings=imagesEnabled=false",
     )
 
     def start(self, splash, max_drivers=None):
-        self._drivers_available = queue.Queue()
-        self._first_driver = threading.Lock()
         self.max_drivers = max_drivers or DriversToScrape.max_drivers
 
         if CREATE_DRIVER_AT_INIT:
             for i in range(self.max_drivers):
                 if splash:
                     splash.update(f"{TXT.driver_creation} {i + 1}/{self.max_drivers}")
-                self.create(self._create_driver)
+                self.create_driver()
 
-    def _create_driver(self, n_drivers):
+    def get_driver_options(self):
         options = webdriver.ChromeOptions()
         options.headless = True
-        options.binary_location = CHROME_EXE_PATH
-
-        for option in self.options + (
-            self.options_V2 if USE_UC_V2 else self.options_V1
-        ):
+        for option in self.options:
             options.add_argument(option)
 
-        # prefs do not work with UC v2 at the moment
-        if not USE_UC_V2:
-            for k, v in self.experimental_options.items():
-                options.add_experimental_option(k, v)
-
-        if n_drivers == 0:
-            with self._first_driver:
-                driver = webdriver.Chrome(options=options)
-
-        else:
-            # block till the 1st driver has been created
-            # to avoid permission error caused by the 1st driver being patched
-            with self._first_driver:
-                pass
-            driver = webdriver.Chrome(options=options)
-
-        self._drivers_available.put(driver)
-        return driver
-
-    def get(self):
-        self.create(self._create_driver)  # done till max_drivers is reached
-        return self._drivers_available.get()
-
-    def dispose(self, driver):
-        self._drivers_available.put(driver)
+        return options
 
 
 class DriversToShow(_DriversHandler):
-    name = "chromedriver to show"
+    name = "Chromedriver (browser)"
 
-    @staticmethod
-    def _create_driver(n_driver):
+    def __init__(self):
+        self._drivers_available = queue.Queue()
+        super().__init__()
+
+    def get_driver_options(self):
         options = webdriver.ChromeOptions()
-        options.binary_location = CHROME_EXE_PATH
         options.add_argument("--start-maximized")
-        return webdriver.Chrome(options=options)
+        return options
 
     def defer(self, show, url):
         threading.Thread(target=self._defer, args=(show, url), daemon=True).start()
 
     @staticmethod
     def _wait_browser_closed(driver):
-        disconnected_msg = (
-            "disconnected: not connected to DevTools"
-        )
+        disconnected_msg = "disconnected: not connected to DevTools"
         while True:
             if msg := driver.get_log("driver"):
                 if disconnected_msg in msg[-1]["message"]:
@@ -202,7 +226,7 @@ class DriversToShow(_DriversHandler):
                     break
 
     def _defer(self, show, url):
-        if driver := self.create(self._create_driver):
+        if driver := self.get():
             log(f"SHOW in {self.name}")
             try:
                 driver.get(url)
