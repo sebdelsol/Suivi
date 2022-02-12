02e9214e3381b8b24313a054631c2e34c04b8aa5
sebdelsol <seb.morin@gmail.com>
2022-02-12 02:13:53 +0100

    clean

--- couriers/__init__.py
+++ couriers/__init__.py
@@ -1,8 +1,6 @@
 from importlib import import_module
-from pathlib import Path
 from pkgutil import iter_modules
 
-# import all modules in the couriers package so that Couriers_classes is populated
-package_dir = Path(__file__).parent
-for (_, module_name, _) in iter_modules([package_dir]):
+# import all modules in the package so that Couriers_classes is populated
+for _, module_name, _ in iter_modules(__path__):
     module = import_module(f"{__name__}.{module_name}")
