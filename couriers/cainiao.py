3f7fd20f9a17a577cb0f5bc5eab416469af44770
sebdelsol <seb.morin@gmail.com>
2022-02-26 13:34:00 +0100

    status_label

--- couriers/cainiao.py
+++ couriers/cainiao.py
@@ -14,7 +14,7 @@ class Cainiao(Courier):
         return f"https://global.cainiao.com/detail.htm?mailNoList={idship}&lang=zh"
 
     #  do not return any selenium objects, the driver is disposed after
-    @Courier.driversToScrape.get(wait_elt_timeout=30)
+    @Courier.driversToScrape.get(wait_elt_timeout=15)
     def get_content(self, idship, driver):
         url = self.get_url_for_browser(idship)
         driver.get(url)
@@ -34,9 +34,9 @@ class Cainiao(Courier):
             slider = driver.wait_for(
                 '//span[@class="nc_iconfont btn_slide"]', EC.element_to_be_clickable
             )
-
-            slide = driver.find_element(
-                By.XPATH, '//div[@class="scale_text slidetounlock"]/span'
+            slide = driver.wait_for(
+                '//div[@class="scale_text slidetounlock"]/span[contains(text(),"slide")]',
+                EC.element_to_be_clickable,
             )
             action = ActionChains(driver)
             action.drag_and_drop_by_offset(slider, slide.size["width"], 0).perform()
@@ -52,10 +52,11 @@ class Cainiao(Courier):
     def parse_content(self, content):
         events = []
 
+        status_label = self.get_txt(content, '//*[@id="waybill_title"]/h3')
         timeline = content.xpath('//ol[@class="waybill-path"]/li')
         for event in timeline:
             txts = event.xpath("./p/text()")
             label, date = txts[:2]
             events.append(dict(date=get_utc_time(date), label=label))
 
-        return events, {}
+        return events, dict(status_label=status_label)
