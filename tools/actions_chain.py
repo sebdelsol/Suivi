cbafd0ba3df1ffe3a63769f24ae572e251bc31fc
sebdelsol <seb.morin@gmail.com>
2022-03-02 13:00:08 +0100

    return actionchains

--- tools/actions_chain.py
+++ tools/actions_chain.py
@@ -3,10 +3,6 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 class EnhancedActionChains(ActionChains):
     def smooth_move_mouse(self, dx, dy, n_step=50):
-        # no pause
-        # pylint: disable=protected-access
-        self.w3c_actions.pointer_action._duration = 1
-
         def get_displacement():
             step_x = dx / n_step
             step_y = dy / n_step
@@ -16,5 +12,10 @@ class EnhancedActionChains(ActionChains):
                     round((i + 1) * step_y) - round(i * step_y),
                 )
 
+        # no pause
+        # pylint: disable=protected-access
+        self.w3c_actions.pointer_action._duration = 1
         for ddx, ddy in get_displacement():
             self.w3c_actions.pointer_action.move_by(ddx, ddy)
+
+        return self
