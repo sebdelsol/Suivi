15565074d6cdaf7aaf749e16bab005a5b8adf177
sebdelsol <seb.morin@gmail.com>
2022-03-07 12:44:54 +0100

    simpler

--- couriers/__init__.py
+++ couriers/__init__.py
@@ -1,6 +1,5 @@
-from importlib import import_module
 from pkgutil import iter_modules
 
 # import all modules in the package to auto-populate Couriers_classes
 for _, module_name, _ in iter_modules(__path__):
-    module = import_module(f"{__name__}.{module_name}")
+    __import__(f"{__name__}.{module_name}")
