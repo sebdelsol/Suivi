b4cb15763c43d28bd75c3be6d62d41fcd29adc49
sebdelsol <seb.morin@gmail.com>
2022-03-12 16:19:11 +0100

    googletrans

--- translation/googleapi.py
+++ translation/googleapi.py
@@ -1,12 +1,16 @@
-from googletranslate import Translator
+from googletrans import Translator
 
-from .translate import TranslationService
+from .translate import SameLanguageError, TranslationService
 
 
 class GoogleAPI(TranslationService):
     def __init__(self, to_lang):
-        self.translator = Translator(to_lang, "auto")
+        self.translator = Translator()
         super().__init__(to_lang)
 
     def translate(self, txt):
-        return self.translator(txt)
+        translation = self.translator.translate(txt, dest=self.to_lang)
+        if translation.src == self.to_lang:
+            raise SameLanguageError
+
+        return translation.text
