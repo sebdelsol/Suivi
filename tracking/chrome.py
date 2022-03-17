2908ae3653fe9632ab8fbc7fda580a6ec56ee7b3
sebdelsol <seb.morin@gmail.com>
2022-03-17 10:56:06 +0100

    fix webdriver.find_chrome_executable()

--- tracking/chrome.py
+++ tracking/chrome.py
@@ -1,8 +1,15 @@
+import os
+
 import undetected_chromedriver as webdriver
 from win32api import HIWORD, GetFileVersionInfo
 
 # needed for being compliant with W3C webdriver shadow-root specs
 CHROME_MIN_VERSION = 96
+
+# fix webdriver.find_chrome_executable()
+# in case PROGRAMFILES(X86) is None on x86 Windows
+if "PROGRAMFILES(X86)" not in os.environ:
+    os.environ["PROGRAMFILES(X86)"] = ""
 
 
 def get_chrome_main_version():
@@ -22,5 +29,6 @@ def check_chrome():
 
     print(f"this app needs chrome >={CHROME_MIN_VERSION}")
     return False
+
 
 Chrome_Version = get_chrome_main_version()
